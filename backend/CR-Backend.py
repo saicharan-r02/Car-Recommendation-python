ce148fd7d57fbc4337735141f1782382297f69ef
saicharan-o <saicharanr227@gmail.com>
2026-04-27 13:30:17 +0530

    errors in CR-Backend file

--- backend/CR-Backend.py
+++ backend/CR-Backend.py
@@ -44,7 +44,7 @@ if __name__ == "__main__":
 
     try:
         print(json.dumps(get_recommendations(float(sys.argv[1]), float(sys.argv[2]))))
+    
     except:
-        
         print(json.dumps([]))
         
